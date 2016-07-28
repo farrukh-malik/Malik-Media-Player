739ea3083464e7f4d4bd1f2f106196c99c1d51bd
farrukh <farrukhmalik46@gmail.com>
2016-07-27 17:45:51 -0700

    MEDIA PLAYER

---
+++ Malik Player/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Malik_Player
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog obj = new OpenFileDialog();
+            obj.Filter = "(*.mp4) | *.mp4";
+
+            if(obj.ShowDialog() == DialogResult.OK){
+
+                axVLCPlugin21.playlist.add(obj.FileName, obj.SafeFileName, null);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            axVLCPlugin21.playlist.play();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            axVLCPlugin21.playlist.stop();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            axVLCPlugin21.playlist.togglePause();
+        }
+    }
+}
